23bdc45d04f6262b0d9981133a05819bbe505452
Andres David <andres.holguin@ufl.edu>
2020-10-08 12:36:04 -0400

    Modify App Layout
    * Use Bootstrap Grid to Layout App
    * Add events to the FullCalendar
    * Remove css from create-react-app

--- src/App.js
+++ src/App.js
@@ -2,15 +2,49 @@ import React from 'react';
 import './App.css';
 import FullCalendar from '@fullcalendar/react'
 import dayGridPlugin from '@fullcalendar/daygrid'
+import { Container, Row, Col } from 'react-bootstrap'
+import SwapList from './Components/SwapList.js'
+
+let todayStr = new Date().toISOString().replace(/T.*$/, '') // YYYY-MM-DD of today
+
+let events = [
+  {
+    //id: createEventId(),
+    title: 'All-day event',
+    start: todayStr
+  },
+  {
+    //id: createEventId(),
+    title: 'Timed event',
+    start: new Date()
+  }
+]
 
 function App() {
+
   return (
-    <div className="App">
-      <FullCalendar
-        plugins={[ dayGridPlugin ]}
-        initialView="dayGridMonth"
-      />
-    </div>
+    <Container fluid>
+      <Row>
+        <Col md="0.5">
+          NavBar
+        </Col>
+        <Col>
+          <div className="App">
+          <FullCalendar
+          plugins={[ dayGridPlugin ]}
+          initialView="dayGridMonth"
+          initialEvents={events}
+          />
+          </div>
+        </Col>
+        <Col md="2">
+          <SwapList></SwapList>
+        </Col>
+        <Col md="1">
+          Filters
+        </Col>
+      </Row>
+    </Container>
   );
 }
 

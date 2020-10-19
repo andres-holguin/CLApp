bea578ff811fe1647d906b84aacd8af170bcea9b
Andres David <andres.holguin@ufl.edu>
2020-10-19 16:24:45 -0400

    index.js: Initialize firebase app

--- src/index.js
+++ src/index.js
@@ -4,6 +4,20 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import * as firebase from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAu5D5ozEkCR-NCE3Pfak3xa3wq_eqgytI",
+  authDomain: "clapp-5ac46.firebaseapp.com",
+  databaseURL: "https://clapp-5ac46.firebaseio.com",
+  projectId: "clapp-5ac46",
+  storageBucket: "clapp-5ac46.appspot.com",
+  messagingSenderId: "1018795609155",
+  appId: "1:1018795609155:web:3d3c20986ca3ce5ba018f6",
+  measurementId: "G-Q2BWFW4MPK"
+};
+
+firebase.initializeApp(firebaseConfig);
 
 ReactDOM.render(
   <React.StrictMode>
